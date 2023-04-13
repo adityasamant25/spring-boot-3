6203ac4524fb08252d35da9a2259c4d1fce4ae87
Aditya Samant <adityasamant.learnings@gmail.com>
2023-04-13 22:04:51 +0530

    Used jdbcTemplate

--- boot3/src/main/java/letscode/boot3/Boot3Application.java
+++ boot3/src/main/java/letscode/boot3/Boot3Application.java
@@ -2,20 +2,22 @@ package letscode.boot3;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.postgresql.Driver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
-
-import org.postgresql.Driver;
-
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Slf4j
 public class Boot3Application {
 
     public static void main(String[] args) {
-        var cs = new DefaultCustomerService();
+        var dataSource = new DriverManagerDataSource("jdbc:postgresql://localhost/postgres", "postgres", "postgres");
+        dataSource.setDriverClassName(Driver.class.getName());
+        var template = new JdbcTemplate(dataSource);
+        template.afterPropertiesSet();
+        var cs = new DefaultCustomerService(template);
         var all = cs.all();
         all.forEach(c -> log.info(c.toString()));
     }
@@ -25,34 +27,17 @@ public class Boot3Application {
 @Slf4j
 class DefaultCustomerService {
 
-    private final DataSource dataSource;
+    private final JdbcTemplate template;
+    private final RowMapper<Customer> customerRowMapper = (resultSet, rowNum) -> new Customer(resultSet.getInt("id"), resultSet.getString("name"));
 
 
-    DefaultCustomerService() {
-        var dataSource = new DriverManagerDataSource("jdbc:postgresql://localhost/postgres", "postgres", "postgres");
-        dataSource.setDriverClassName(Driver.class.getName());
-        this.dataSource = dataSource;
+    DefaultCustomerService(JdbcTemplate template) {
+
+        this.template = template;
     }
 
     Collection<Customer> all() {
-        var listOfCustomers = new ArrayList<Customer>();
-        try {
-            try (var connection = this.dataSource.getConnection()) {
-                try (var stmt = connection.createStatement()) {
-                    try (var resultSet = stmt.executeQuery("select * from customers")) {
-                        while (resultSet.next()) {
-                            var name = resultSet.getString("name");
-                            var id = resultSet.getInt("id");
-                            listOfCustomers.add(new Customer(id, name));
-                        }
-                    }
-                }
-            }
-        } catch (Exception th) {
-            log.error("something went terribly wrong, but search me, I have no idea what!", th);
-        }
-
-        return listOfCustomers;
+        return this.template.query("select id, name from customers", this.customerRowMapper);
     }
 
 }
